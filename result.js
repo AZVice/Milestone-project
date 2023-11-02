eee3ec20e5bcd38f1e0cb876f8b38b6733787e25
Carlos Hernandez <chernandezmiranda@gmail.com>
2023-11-01 17:47:18 -0700

    added sound.

--- result.js
+++ result.js
@@ -13,38 +13,42 @@ function setBetAmount(buttonId){
     }
 }
 
-const buttonClickSound = document.getElementById("buttonClickSound");
-buttonClickSound.play();
 
 
 function winnerCheck(){
-    if (cardOneDiv.textContent === cardTwoDiv.textContent && cardTwoDiv.textContent === cardThreeDiv.textContent) {
-        if (cardOneDiv.textContent === 7) {
-            currentScore *= 10;
+    if (cardOneDiv.textContent === cardTwoDiv.textContent && cardTwoDiv.textContent === cardThreeDiv.textContent){
+        if(cardOneDiv.textContent === "7") {
             resultContainer.textContent = "Jackpot";
             const winnerPageURL = "winnerPage.html";
             const winWindow = window.open(winnerPageURL, '_blank');
-            spin.disabled = true;
+            currentScore *= 10;
+            document.getElementById("jackpotSound").play();
+            spin1.disabled = true;
+            spin2.disabled = true;
+            spin3.disabled = true;
 
             setTimeout(function() {
                 winWindow.close();
-                spin.disabled = false;
-            }, 6000);
+                spin1.disabled = false;
+                spin2.disabled = false;
+                spin3.disabled = false;
+        }, 6000);
+    
+            
         }else {
             currentScore += betAmount * 6;
-            resultContainer.textContent =("You Won X6")
-        };
-        /*
-    } else if (cardOneDiv.textContent === cardTwoDiv.textContent && cardTwoDiv.textContent === cardThreeDiv.textContent && cardOneDiv.textContent !== 7){
-        currentScore += betAmount * 6;
-        resultContainer.textContent =("You Won X6");
-        */
-    }else if (cardOneDiv.textContent === cardTwoDiv.textContent || cardOneDiv.textContent === cardThreeDiv.textContent || cardTwoDiv.textContent === cardThreeDiv.textContent) {
-                currentScore += betAmount * 4;
-                resultContainer.textContent = "You Won X4";
+            document.getElementById("winnerX6Sound").play();
+            resultContainer.textContent =("You Won X6");
+        }
+        
+    }else if (cardOneDiv.textContent === cardTwoDiv.textContent || cardOneDiv.textContent === cardThreeDiv.textContent || cardTwoDiv.textContent == cardThreeDiv.textContent) {
+        currentScore += betAmount * 4;
+        document.getElementById("winnerX4Sound").play();
+        resultContainer.textContent = "You Won X4";
     } else {
-                resultContainer.textContent = "Try again!";
-    }
+        resultContainer.textContent = "Try again!";
+    }        
+    
 
     updateCreditScreen();
 }
@@ -54,3 +58,5 @@ function addCredits(){
     updateCreditScreen();
 }
 */
+
+//cardOneDiv.textContent === cardTwoDiv.textContent || cardOneDiv.textContent === cardThreeDiv.textContent || cardTwoDiv.textContent === cardThreeDiv.textContent)
